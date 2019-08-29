1b8f198c89993403ea81940e3eb23d9c9214cfcf
Daevon <31278230+Oniketsu@users.noreply.github.com>
2019-08-29 13:29:48 -0400

    Update ubuntu_start.sh

--- ubuntu_start.sh
+++ ubuntu_start.sh
@@ -8,56 +8,404 @@ exec 1>log.out 2>&1
 
 # Clean and update
 sudo apt-get clean
-sudo apt-get update -q
+sudo apt-get update
 sudo apt-get dist-upgrade - y
 
-#Decrease swap usage
-echo "#decrease swap usage to a more reasonable level
-vm.swappiness=10" >> /etc/sysctl.conf
-
-#Add repos
-sudo add-apt-repository ppa:webupd8team/java - y
-sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
-sudo add-apt-repository ppa:ubuntu-wine/ppa -y
-sudo add-apt-repository ppa:nilarimogard/webupd8 -y
-sudo add-apt-repository ppa:mscore-ubuntu/mscore-stable -y
-sudo add-apt-repository ppa:danielrichter2007/grub-customizer -y
-
-sudo apt-get update -q
 
 
 #Install programs
-sudo apt-get install git geany python3-pip build-essential libssl-dev libffi-dev python-dev aptitude evolution gimp geany minetest freecad vlc clementine clamav ntfs-3g synaptic dconf-tools gksu gdebi inxi leafpad catfish pavucontrol terminator cmatrix exfat-utils john tcptrack slurm geany thunderbird tor chromium-browser gdebi-core gcc build-essential yum libwxbase3.0-dev libwxgtk3.0-dev musescore grub-customizer unity-tweak-tool exfat-fuse ubuntu-restricted-extras dconf-tools p7zip-rar gparted gnome-session-flashback oracle-java8-installer tree wireshark nessus wine1.8 winetricks chkrootkit snort kismet winusb etherape zenmap -y
+sudo apt-get install git geany python3-pip build-essential libssl-dev libffi-dev python-dev aptitude evolution gimp geany minetest freecad vlc clementine clamav ntfs-3g synaptic dconf-tools gdebi inxi leafpad catfish pavucontrol terminator cmatrix exfat-utils john tcptrack slurm geany thunderbird tor chromium-browser gdebi-core gcc build-essential yum libwxbase3.0-dev libwxgtk3.0-dev musescore grub-customizer unity-tweak-tool exfat-fuse ubuntu-restricted-extras dconf-tools p7zip-rar gparted gnome-session-flashback tree wireshark wine1.8 winetricks chkrootkit snort kismet etherape zenmap net-tools -y
 
-#minimize on click
-gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ launcher-minimize-window true
+# Update again
+sudo apt-get clean
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get clean
+sudo apt-get update
+sudo apt-get dist-upgrade -y
 
-#Update, fix broke/missing
+# Add alias'
+touch customrc.txt
+echo "# ~/.bashrc: executed by bash(1) for non-login shells.
+# see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
+# for examples
 
-sudo aptitude clean
-sudo aptitude update
-sudo apt-get install -f
-sudo aptitude dist-upgrade -y
+# If not running interactively, don't do anything
+case $- in
+    *i*) ;;
+      *) return;;
+esac
 
-#install teamviewer
-wget https://downloadus2.teamviewer.com/download/version_12x/teamviewer_12.0.71510_i386.deb
-sudo gdebi teamviewer*.deb 
-rm -rf teamviewer*.deb
+export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/usr/share/games:/usr/local/sbin:/usr/sbin:/sbin:~/.local/bin:/snap/bin:$PATH
 
-#install Atom.io
-wget https://atom.io/download/deb
-sudo gdebi deb
-rm -rf deb
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
 
-#Final clean up using aptitude
-sudo aptitude clean
-sudo aptitude update
-sudo aptitude dist-upgrade -y
+# append to the history file, don't overwrite it
+shopt -s histappend
 
-#Decrease Swap usage to a workable level
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000
+HISTFILESIZE=2000
 
-sudo echo "# Decrease swap usage to a more reasonable level
-vm.swappiness=10" >> /etc/sysctl.conf
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# If set, the pattern "**" used in a pathname expansion context will
+# match all files and zero or more directories and subdirectories.
+#shopt -s globstar
+
+# make less more friendly for non-text input files, see lesspipe(1)
+#[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+# set a fancy prompt (non-color, unless we know we "want" color)
+case "$TERM" in
+    xterm-color) color_prompt=yes;;
+esac
+
+# uncomment for a colored prompt, if the terminal has the capability; turned
+# off by default to not distract the user: the focus in a terminal window
+# should be on the output of commands, not on the prompt
+force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+	# We have color support; assume it's compliant with Ecma-48
+	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+	# a case would tend to support setf rather than setaf.)
+	color_prompt=yes
+    else
+	color_prompt=
+    fi
+fi
+
+if [ "$color_prompt" = yes ]; then
+    PS1="\[\033[0;31m\]\342\224\214\342\224\200\$([[ \$? != 0 ]] && echo \"[\[\033[0;31m\]\342\234\227\[\033[0;37m\]]\342\224\200\")[$(if [[ ${EUID} == 0 ]]; then echo '\[\033[01;31m\]root\[\033[01;33m\]@\[\033[01;96m\]\h'; else echo '\[\033[0;39m\]\u\[\033[01;33m\]@\[\033[01;96m\]\h'; fi)\[\033[0;31m\]]\342\224\200[\[\033[0;32m\]\w\[\033[0;31m\]]\n\[\033[0;31m\]\342\224\224\342\224\200\342\224\200\342\225\274 \[\033[0m\]\[\e[01;33m\]\\$\[\e[0m\]"
+else
+    PS1='┌──[\u@\h]─[\w]\n└──╼ \$ '
+fi
+
+# Set 'man' colors
+if [ "$color_prompt" = yes ]; then
+	man() {
+	env \
+	LESS_TERMCAP_mb=$'\e[01;31m' \
+	LESS_TERMCAP_md=$'\e[01;31m' \
+	LESS_TERMCAP_me=$'\e[0m' \
+	LESS_TERMCAP_se=$'\e[0m' \
+	LESS_TERMCAP_so=$'\e[01;44;33m' \
+	LESS_TERMCAP_ue=$'\e[0m' \
+	LESS_TERMCAP_us=$'\e[01;32m' \
+	man "$@"
+	}
+fi
+
+unset color_prompt force_color_prompt
+
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+xterm*|rxvt*)
+    PS1="\[\033[0;31m\]\342\224\214\342\224\200\$([[ \$? != 0 ]] && echo \"[\[\033[0;31m\]\342\234\227\[\033[0;37m\]]\342\224\200\")[$(if [[ ${EUID} == 0 ]]; then echo '\[\033[01;31m\]root\[\033[01;33m\]@\[\033[01;96m\]\h'; else echo '\[\033[0;39m\]\u\[\033[01;33m\]@\[\033[01;96m\]\h'; fi)\[\033[0;31m\]]\342\224\200[\[\033[0;32m\]\w\[\033[0;31m\]]\n\[\033[0;31m\]\342\224\224\342\224\200\342\224\200\342\225\274 \[\033[0m\]\[\e[01;33m\]\\$\[\e[0m\]"
+    ;;
+*)
+    ;;
+esac
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    alias dir='dir --color=auto'
+    alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# some more ls aliases
+alias ll='ls -lh'
+alias la='ls -lha'
+alias l='ls -CF'
+alias em='emacs -nw'
+alias dd='dd status=progress'
+alias _='sudo'
+alias _i='sudo -i'
+alias updatea='sudo aptitude clean && sudo aptitude update && sudo aptitude dist-upgrade -y'
+alias updatey='sudo apt-get clean && sudo apt-get update && sudo apt-get upgrade -y'
+alias ..='cd ../..'
+alias ...='cd ../../..'
+alias ....='cd ../../../../'
+alias .....='cd ../../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../..'
+alias untar='tar -zxvf'
+alias wget='wget -c'
+alias getpass="openssl rand -base64 20"
+alias sha='shasum -a 256 '
+alias ping='ping -c 5'
+alias www='python -m SimpleHTTPServer 8000'
+alias speed='speedtest-cli --server 2406 --simple'
+alias ipe='curl ipinfo.io/ip'
+alias c='clear'
+alias bc='bc -l'
+alias sha1='openssl sha1'
+alias mkdir='mkdir -pv'
+alias mount='mount |column -t'
+alias h='history'
+alias j='jobs -l'
+alias path='echo -e ${PATH//:/\\n}'
+alias now='date +"%T"'
+alias nowtime=now
+alias nowdate='date +"%d-%m-%Y"'
+alias vi=vim
+alias sna='sudo nano'
+alias vis='vim "+set si"'
+alias edit='nano'
+alias ports='netstat -tulanp'
+alias hs='history | grep'
+alias mkcd='foo(){ mkdir -p "$1"; cd "$1" }; foo '
+alias myip="curl http://ipecho.net/plain; echo"
+alias d='date +%F'
+alias now='date +"%T"'
+alias nowtime=now
+alias nowdate='date +"%m-%d-%Y"'
+alias install='sudo apt-get install'
+alias installa='sudo aptitude install'
+alias dus='f(){ du -h -d 1 "$@" | sort -h; unset -f f; }; f'
+alias sdus='f(){ sudo du -h -d 1 "$@" | sort -h; unset -f f; }; f'
+alias clr='clear'
+alias logout="gnome-session-quit"
+
+# Use the column command to create pretty tables.
+alias ct='column -t'
+alias dfc='df -hPT | column -t'
+alias mount='mount | column -t'
+
+
+## replace mac with your actual server mac address #
+#alias wakeupnas01='/usr/bin/wakeonlan 00:11:32:11:15:FC'
+#alias wakeupnas02='/usr/bin/wakeonlan 00:11:32:11:15:FD'
+#alias wakeupnas03='/usr/bin/wakeonlan 00:11:32:11:15:FE'
+
+
+## shortcut  for iptables and pass it via sudo#
+alias ipt='sudo /sbin/iptables'
+
+# display all rules #
+alias iptlist='sudo /sbin/iptables -L -n -v --line-numbers'
+alias iptlistin='sudo /sbin/iptables -L INPUT -n -v --line-numbers'
+alias iptlistout='sudo /sbin/iptables -L OUTPUT -n -v --line-numbers'
+alias iptlistfw='sudo /sbin/iptables -L FORWARD -n -v --line-numbers'
+alias firewall=iptlist
+
+#New command Set
+alias path='echo -e ${PATH//:/\\n}'
+alias now='date +"%T"'
+alias nowtime=now
+alias nowdate='date +"%d-%m-%Y"'
+
+
+# get web server headers #
+alias header='curl -I'
+
+# find out if remote server supports gzip / mod_deflate or not #
+alias headerc='curl -I --compress'
+
+#Safety nets
+# do not delete / or prompt if deleting more than 3 files at a time #
+alias rm='rm -I --preserve-root'
+ 
+# confirmation #
+alias mv='mv -i'
+alias cp='cp -i'
+alias ln='ln -i'
+ 
+# Parenting changing perms on / #
+alias chown='chown --preserve-root'
+alias chmod='chmod --preserve-root'
+alias chgrp='chgrp --preserve-root'
+
+# become root #
+alias root='sudo -i'
+alias su='sudo -i'
+
+
+#Pass halt/reboot via sudo
+# reboot / halt / poweroff
+alias reboot='sudo /sbin/reboot'
+alias poweroff='sudo /sbin/poweroff'
+alias halt='sudo /sbin/halt'
+alias shutdown='sudo /sbin/shutdown'
+
+#Get Memory Stats
+
+## pass options to free ##
+alias meminfo='free -m -l -t'
+
+## get top process eating memory
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+
+## get top process eating cpu ##
+alias pscpu='ps auxf | sort -nr -k 3'
+alias pscpu10='ps auxf | sort -nr -k 3 | head -10'
+
+alias meminfo='free -m -l -t'
+
+#Control Web Servers
+# also pass it via sudo so whoever is admin can reload it without calling you #
+alias nginxreload='sudo /usr/local/nginx/sbin/nginx -s reload'
+alias nginxtest='sudo /usr/local/nginx/sbin/nginx -t'
+alias lightyload='sudo /etc/init.d/lighttpd reload'
+alias lightytest='sudo /usr/sbin/lighttpd -f /etc/lighttpd/lighttpd.conf -t'
+alias httpdreload='sudo /usr/sbin/apachectl -k graceful'
+alias httpdtest='sudo /usr/sbin/apachectl -t && /usr/sbin/apachectl -t -D DUMP_VHOSTS'
+
+#Alias into our backup stuff
+# if cron fails or if you want backup on demand just run these commands #
+# again pass it via sudo so whoever is in admin group can start the job #
+# Backup scripts #
+alias backup='sudo /home/scripts/admin/scripts/backup/wrapper.backup.sh --type local --taget /raid1/backups'
+alias nasbackup='sudo /home/scripts/admin/scripts/backup/wrapper.backup.sh --type nas --target nas01'
+alias s3backup='sudo /home/scripts/admin/scripts/backup/wrapper.backup.sh --type nas --target nas01 --auth /home/scripts/admin/.authdata/amazon.keys'
+alias rsnapshothourly='sudo /home/scripts/admin/scripts/backup/wrapper.rsnapshot.sh --type remote --target nas03 --auth /home/scripts/admin/.authdata/ssh.keys --config /home/scripts/admin/scripts/backup/config/adsl.conf'
+alias rsnapshotdaily='sudo  /home/scripts/admin/scripts/backup/wrapper.rsnapshot.sh --type remote --target nas03 --auth /home/scripts/admin/.authdata/ssh.keys  --config /home/scripts/admin/scripts/backup/config/adsl.conf'
+alias rsnapshotweekly='sudo /home/scripts/admin/scripts/backup/wrapper.rsnapshot.sh --type remote --target nas03 --auth /home/scripts/admin/.authdata/ssh.keys  --config /home/scripts/admin/scripts/backup/config/adsl.conf'
+alias rsnapshotmonthly='sudo /home/scripts/admin/scripts/backup/wrapper.rsnapshot.sh --type remote --target nas03 --auth /home/scripts/admin/.authdata/ssh.keys  --config /home/scripts/admin/scripts/backup/config/adsl.conf'
+alias amazonbackup=s3backup
+
+#23: Desktop specific – play avi/mp3 files on demand
+## play video files in a current directory ##
+# cd ~/Download/movie-name
+# playavi or vlc
+alias playavi='mplayer *.avi'
+alias vlc='vlc *.avi'
+ 
+# play all music files from the current directory #
+alias playwave='for i in *.wav; do mplayer "$i"; done'
+alias playogg='for i in *.ogg; do mplayer "$i"; done'
+alias playmp3='for i in *.mp3; do mplayer "$i"; done'
+ 
+# play files from nas devices #
+alias nplaywave='for i in /nas/multimedia/wave/*.wav; do mplayer "$i"; done'
+alias nplayogg='for i in /nas/multimedia/ogg/*.ogg; do mplayer "$i"; done'
+alias nplaymp3='for i in /nas/multimedia/mp3/*.mp3; do mplayer "$i"; done'
+ 
+# shuffle mp3/ogg etc by default #
+alias music='mplayer --shuffle *'
+
+#24: Set default interfaces for sys admin related commands
+## All of our servers eth1 is connected to the Internets via vlan / router etc  ##
+alias dnstop='dnstop -l 5  eth1'
+alias vnstat='vnstat -i eth1'
+alias iftop='iftop -i eth1'
+alias tcpdump='tcpdump -i eth1'
+alias ethtool='ethtool eth1'
+
+# work on wlan0 by default #
+# Only useful for laptop as all servers are without wireless interface
+alias iwconfig='iwconfig wlan0'
+
+#25: Get system memory, cpu usage, and gpu memory info quickly
+## pass options to free ##
+alias meminfo='free -m -l -t'
+
+## get top process eating memory
+alias psmem='ps auxf | sort -nr -k 4'
+alias psmem10='ps auxf | sort -nr -k 4 | head -10'
+
+## get top process eating cpu ##
+alias pscpu='ps auxf | sort -nr -k 3'
+alias pscpu10='ps auxf | sort -nr -k 3 | head -10'
+
+## Get server cpu info ##
+alias cpuinfo='lscpu'
+
+## older system use /proc/cpuinfo ##
+##alias cpuinfo='less /proc/cpuinfo' ##
+
+## get GPU ram on desktop / laptop##
+alias gpumeminfo='grep -i --color memory /var/log/Xorg.0.log'
+
+#26: Control Home Router
+# Reboot my home Linksys WAG160N / WAG54 / WAG320 / WAG120N Router / Gateway from *nix.
+#alias rebootlinksys="curl -u 'admin:my-super-password' 'http://192.168.1.2/setup.cgi?todo=reboot'"
+
+# Reboot tomato based Asus NT16 wireless bridge
+#alias reboottomato="ssh admin@192.168.1.1 /sbin/reboot"
+
+#28 Use different browser for testing website
+## this one saved by butt so many times ##
+alias ff4='/opt/firefox4/firefox'
+alias ff13='/opt/firefox13/firefox'
+alias chrome='/opt/google/chrome/chrome'
+alias opera='/opt/opera/opera'
+
+#default ff
+alias ff=ff13
+
+#my default browser
+alias browser=chrome
+
+#30: It’s your turn to share…
+## set some other defaults ##
+alias df='df -H'
+alias du='du -ch'
+
+# top is atop, just like vi is vim
+alias top='atop'
+
+## nfsrestart  - must be root  ##
+## refresh nfs mount / cache etc for Apache ##
+alias nfsrestart='sync && sleep 2 && /etc/init.d/httpd stop && umount netapp2:/exports/http && sleep 2 && mount -o rw,sync,rsize=32768,wsize=32768,intr,hard,proto=tcp,fsc natapp2:/exports /http/var/www/html &&  /etc/init.d/httpd start'
+
+## Memcached server status  ##
+alias mcdstats='/usr/bin/memcached-tool 10.10.27.11:11211 stats'
+alias mcdshow='/usr/bin/memcached-tool 10.10.27.11:11211 display'
+
+## quickly flush out memcached server ##
+alias flushmcd='echo "flush_all" | nc 10.10.27.11 11211'
+
+## Remove assets quickly from Akamai / Amazon cdn ##
+alias cdndel='/home/scripts/admin/cdn/purge_cdn_cache --profile akamai'
+alias amzcdndel='/home/scripts/admin/cdn/purge_cdn_cache --profile amazon'
+
+## supply list of urls via file or stdin
+alias cdnmdel='/home/scripts/admin/cdn/purge_cdn_cache --profile akamai --stdin'
+alias amzcdnmdel='/home/scripts/admin/cdn/purge_cdn_cache --profile amazon --stdin'
+
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi" 
+>> customrc.txt
+
+mv customrc.txt ~/.bashrc
+
+source ~/.bashrc
 
 #Reboot system
 
