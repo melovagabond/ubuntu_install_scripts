12c6f6fd754d1783cc57feeebfdac5bf13e84243
Daevon <31278230+Oniketsu@users.noreply.github.com>
2020-01-17 00:38:59 -0500

    Create repos.sh
    collection of git repos to install at /opt
    Optional tmux customization repo installation

---
+++ repos.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# install git
+apt-get update
+apt-get install git
+
+# Repos to install
+cd /opt
+
+git clone https://github.com/Greenwolf/Spray.git
+
+git clone https://github.com/Greenwolf/social_mapper.git
+
+git clone https://github.com/Greenwolf/social_attacker.git
+
+git clone https://github.com/swisskyrepo/PayloadsAllTheThings.git payloads
+
+git clone https://github.com/s0wr0b1ndef/PayloadsAllTheThings.git payloads1
+
+rsync -a payloads1 payloads
+
+rm -rf payloads1
+
+git clone https://github.com/iphelix/dnschef.git
+
+git clone https://github.com/sqlmapproject/sqlmap.git
+
+git clone clone https://github.com/hak5darren/USB-Rubber-Ducky
+
+git clone https://github.com/hak5/bashbunny-payloads.git
+
+git clone https://github.com/hak5/packetsquirrel-payloads.git
+
+git clone https://github.com/hak5/wifipineapple-modules.git
+
+git clone https://github.com/hak5/lanturtle-modules.git
+
+git clone https://github.com/hak5/signalowl-payloads.git
+
+git clone https://github.com/hak5/sharkjack-payloads.git
+
+git clone https://github.com/infodox/python-pty-shells.git
+
+git clone https://github.com/jaws/jaws.git
+
+git clone https://github.com/mame82/P4wnP1.git
+
+git clone https://github.com/aboul3la/Sublist3r.git
+
+git clone https://github.com/TheRook/subbrute.git
+
+git clone https://github.com/ZerBea/hcxtools.git
+
+# Customize tmux
+cd
+
+apt-get install tmux
+
+git clone https://github.com/gpakosz/.tmux.git
+
+ln -s -f .tmux/.tmux.conf
+
+cp .tmux/.tmux.conf.local .
